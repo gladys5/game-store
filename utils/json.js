ed1ddb18d8d8d80201e27e344b90b6f200bce3e0
gladys5 <yumanji@live.com>
2022-07-04 08:29:05 -0600

    appli gitignore

---
+++ utils/json.js
@@ -0,0 +1,169 @@
+// {
+//     "consoles": [
+//         {
+//
+//             "name": "Xbox 360",
+//             "company": "microsoft"
+//
+//         }
+//         {
+//             "id": 2,
+//             "name": "PlayStation 3",
+//             "company": "Sony Computer Entertainment"
+//
+//         }
+//         {
+//
+//             "name": "wii",
+//             "company": "nintendo"
+//
+//         }
+//         {
+//
+//             "name": "PlayStation 4",
+//             "company": "Sony Entertainment"
+//
+//         }
+//         {
+//
+//             "name": "PlayStation 5",
+//             "company": "Sony Entertainment"
+//
+//         }
+//         {
+//
+//             "name": "nintendo Switch",
+//             "company": "nintendo",
+//
+//         }
+//         {
+//             "name": "Wii U",
+//             "company": "nintendo"
+//
+//         }
+//         {
+//
+//             "name": "nintendo 64",
+//             "company": "nintendo"
+//
+//         }
+//     ]
+// }
+//===========================================
+// "games": [
+//         {
+//
+//             "title": "super mario fabricante",
+//             "genre": "editor de niveles"
+//
+//         }
+//         {
+//
+//             "title": "super Smash",
+//             "genre": "lucha"
+//
+//         }
+//         {
+//
+//             "title": "super mario bros.U",
+//             "genre": "plataforma lateral"
+//
+//         }
+//         {
+//
+//             "title": "Don't Starve",
+//             "genre": "supervivencia"
+//
+//         }
+//         {
+//
+//             "title": "Fallout 4",
+//             "genre": "accion"
+//
+//         }
+//         {
+//
+//             "title": "Boku no Natsuyasumi",
+//             "genre": "aventura"
+//
+//         }
+//         {
+//
+//             "title": "Ghosts 'n Goblins",
+//             "genre": "plataformas de lansamiento lateral"
+//
+//         }
+//         {
+//
+//             "title": "farming simulator",
+//             "genre": "simulacion"
+//
+//
+//         }
+//         {
+//
+//             "title": "A Hat in Time",
+//             "genre": "aventura"
+//
+//         }
+//     ]
+// }
+/** "user": [
+        {
+           
+            "name": "Roberto consolas",
+            "email": "roberto@hotmail.com",
+            "password": "password1234"
+           
+        }
+        {
+            
+            "name": "marina salazar",
+            "email": "marina@hotmail.com",
+            "password": "password12345"
+           
+        },
+        {
+            
+            "name": "claudia espino",
+            "email": "claudia@hotmail.com",
+            "password": "password123456"
+            
+        }
+        {
+            
+            "name": "teresa rios",
+            "email": "rios@hotmail.com",
+            "password": "password1234567"
+            
+        }
+        {
+            
+            "name": "gabino comal",
+            "email": "comal@hotmail.com",
+            "password": "password1234567"
+          
+        }
+        {
+            
+            "name": "patricia alboran",
+            "email": "pati@hotmail.com",
+            "password": "password123456789"
+           
+        }
+        {
+            
+            "name": "camilo cuenca",
+            "email": "cuenca@hotmail.com",
+            "password": "password1234567890"
+           
+        }
+        {
+            "id": 8,
+            "name": "tolomeo robles",
+            "email": "robles@hotmail.com",
+            "password": "password12345678901"
+           
+        }
+    ]
+} */
